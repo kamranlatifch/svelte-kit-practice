dc5c2efc22258e3d3de7bbc4fc7c42002084b6c8
kamranlatifch <kamranlatif98@gmail.com>
2024-08-27 14:36:41 +0500

    13: root layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<!-- Main Layout Component -->
+<main class="layout-container">
+	<header class="layout-header">Header</header>
+	<main class="layout-content">
+		<slot />
+	</main>
+	<footer class="layout-footer">Footer</footer>
+</main>
+
+<style>
+	html,
+	body {
+		height: 100%;
+		margin: 0;
+	}
+
+	.layout-container {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.layout-header {
+		background-color: #264653;
+		color: #f4a261;
+		font-size: 30px;
+		text-align: center;
+		padding: 10px;
+	}
+
+	.layout-content {
+		flex: 1;
+		text-align: center;
+	}
+
+	.layout-footer {
+		background-color: #264653;
+		color: #f4a261;
+		font-size: 30px;
+		text-align: center;
+		padding: 10px;
+	}
+</style>
---
+++ src/routes/products/[productId=integer]/+layout.svelte
@@ -0,0 +1,4 @@
+<h3>Featured Product Header</h3>
+
+<slot />
+<h3>Featured Product Footer</h3>
