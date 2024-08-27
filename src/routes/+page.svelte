51490e48fc28c0ef88f678fac19d9b9d6f6bd248
kamranlatifch <kamranlatif98@gmail.com>
2024-08-27 14:17:40 +0500

    11: navigationg programatically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,22 @@
+<!-- Navigating Programatically -->
+
+<script>
+	import { goto, beforeNavigate, afterNavigate } from '$app/navigation';
+	const handleClick = () => {
+		console.log('Order is succesfully placed');
+		goto('/products');
+	};
+	beforeNavigate((navigation) => {
+		console.log({ before: navigation });
+	});
+	afterNavigate((navigation) => {
+		console.log({ after: navigation });
+	});
+</script>
+
 <h1>Welcome Home....</h1>
 
 <a href="/blog">Blog</a>
 <a href="/products">Products</a>
+<!-- Navigating Programatically -->
+<button on:click={handleClick}>Place Order</button>
