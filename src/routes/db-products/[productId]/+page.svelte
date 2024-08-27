e951ee935e090fe25d69aabd346f65a19fc85a01
kamranlatifch <kamranlatif98@gmail.com>
2024-08-27 17:54:13 +0500

    29: using url data get 1 product [productId]

---
+++ src/routes/db-products/[productId]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let data;
+	const product = data.product;
+</script>
+
+<a href="/">Home Page</a>
+
+<h1>{data.title}</h1>
+<div>
+	<h2>{product.title}</h2>
+	<p>{product.description}</p>
+	<span>Price: {product.price}</span>
+</div>
