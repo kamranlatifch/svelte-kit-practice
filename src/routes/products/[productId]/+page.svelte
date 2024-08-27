8338c506e0bd5191d455eef556b6cc41989cdfcd
kamranlatifch <kamranlatif98@gmail.com>
2024-08-27 12:51:34 +0500

    Dynamic Routes

---
+++ src/routes/products/[productId]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+
+	const productId = $page.params.productId;
+	console.log('Page Params are', $page.params);
+</script>
+
+<h1>Detail About Product {productId}</h1>
