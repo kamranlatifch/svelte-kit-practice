e0ffe18d355f0491c3191a8ae81a529aff1e58ba
kamranlatifch <kamranlatif98@gmail.com>
2024-08-27 12:58:07 +0500

    Nested Dynamic Routes

---
+++ src/routes/products/[productId]/review/[reviewId]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+	const { productId, reviewId } = $page.params;
+
+	console.log('Page Params are', $page.params);
+</script>
+
+<h1>Product {productId} and review {reviewId}</h1>
