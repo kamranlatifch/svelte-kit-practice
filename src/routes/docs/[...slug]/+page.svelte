b4b3f90ff474518258822a7f3a3eadb3f69a41fb
kamranlatifch <kamranlatif98@gmail.com>
2024-08-27 13:43:38 +0500

    catch all routes

---
+++ src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { page } from '$app/stores';
+	console.log('SLUG ONES ARE', $page.params.slug);
+	const slugArray = $page.params.slug.split('/');
+</script>
+
+<h1>Docs Home Page</h1>
+
+{#if slugArray?.length === 1}
+	<h1>Viewing Docs for Feature {slugArray[0]}</h1>
+{:else if slugArray?.length === 2}
+	<h1>Viewing Docs for Feature {slugArray[0]} and concept {slugArray[1]}</h1>
+{/if}
