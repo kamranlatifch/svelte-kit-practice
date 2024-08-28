e206aeb50b1b9215e51f50d08810849d29716669
kamranlatifch <kamranlatif98@gmail.com>
2024-08-28 15:49:49 +0500

    40: preload programatically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <!-- Navigating Programatically -->
 
 <script>
-	import { goto, beforeNavigate, afterNavigate } from '$app/navigation';
+	import { goto, preloadData, beforeNavigate, afterNavigate } from '$app/navigation';
 	const handleClick = () => {
 		console.log('Order is succesfully placed');
 		goto('/products');
@@ -15,18 +15,36 @@
 </script>
 
 <h1 class="tall">Welcome Home....</h1>
+<div class="main">
+	<a href="/blog">Blog</a>
+	<a href="/products" data-sveltekit-preload-data="hover">Pre-load-data: Products</a>
+	<a href="/db-products" data-sveltekit-preload-code="hover">DB PreLoad-code Products</a>
+	<a href="/stocks" data-sveltekit-reload>Stocks Reload</a>
+	<a href="/db-products" data-sveltekit-noscroll="hover">DB No Scroll Products</a>
 
-<a href="/blog">Blog</a>
-<a href="/products" data-sveltekit-preload-data="hover">Pre-load-data: Products</a>
-<a href="/db-products" data-sveltekit-preload-code="hover">DB PreLoad-code Products</a>
-<a href="/stocks" data-sveltekit-reload>Stocks Reload</a>
-<a href="/db-products" data-sveltekit-noscroll="hover">DB No Scroll Products</a>
+	<!-- Navigating Programatically -->
+	<button on:click={handleClick}>Place Order</button>
 
-<!-- Navigating Programatically -->
-<button on:click={handleClick}>Place Order</button>
-<!-- 
+	<!-- Preload Programatically -->
+
+	<button
+		on:focus={async () => {
+			await preloadData('/db-products');
+		}}
+		on:mouseover={async () => {
+			await preloadData('/db-products');
+		}}
+		on:click={() => {
+			goto('/db-products');
+		}}>Preload Prog DB Products</button
+	>
+</div>
+
 <style>
-	.tall {
+	/* .tall {
 		height: 100vh;
+	} */
+	.main {
+		display: grid;
 	}
-</style> -->
+</style>
