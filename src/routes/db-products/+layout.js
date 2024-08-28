cb6555984a35b982848b5d06743de6d5084022cf
kamranlatifch <kamranlatif98@gmail.com>
2024-08-28 11:13:33 +0500

    30: Errors and redirects

---
+++ src/routes/db-products/+layout.js
@@ -0,0 +1,5 @@
+export const load = async (loadEvent) => {
+	const { fetch } = loadEvent;
+	const title = 'Featured Products';
+	const response = await fetch('http://localhost:4000/featured-products');
+};
--- src/routes/db-products/[productId]/+page.server.js
+++ src/routes/db-products/[productId]/+page.server.js
@@ -1,11 +1,17 @@
+import { error } from '@sveltejs/kit';
 export const load = async (serverLoadEvent) => {
-	const { fetch, params } = serverLoadEvent;
+	const { fetch, params, url, route } = serverLoadEvent;
 	const { productId } = params;
+	if (productId > 3) {
+		throw error(404, {
+			message: 'Oh no! Looks like the product is currently unavailable.',
+			hint: 'Try a different product'
+		});
+	}
 	const title = 'Product Details';
 	const response = await fetch(`http://localhost:4000/products/${productId}`);
 
 	const product = await response.json();
-
 	return {
 		title,
 		product
