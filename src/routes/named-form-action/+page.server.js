69bdef2e9429292de74936fc1001fdab9f7cc3b3
kamranlatifch <kamranlatif98@gmail.com>
2024-08-29 13:08:12 +0500

    50: form action validation

--- src/routes/named-form-action/+page.server.js
+++ src/routes/named-form-action/+page.server.js
@@ -1,12 +1,18 @@
+import { fail } from '@sveltejs/kit';
+
 export const actions = {
 	login: async ({ request, cookies }) => {
 		const data = await request.formData();
 		const username = data.get('username');
 		const password = data.get('password');
 		if (!username || !password) {
-			return {
-				message: 'Missing Username OR Password for Login'
-			};
+			// return {
+			// 	message: 'Missing Username OR Password for Login'
+			// };
+			return fail(400, {
+				username,
+				message: 'Fail Missing Username OR Password for Login'
+			});
 		}
 
 		cookies.set('username', username, { path: '/' });
@@ -17,9 +23,13 @@ export const actions = {
 		const username = data.get('username');
 		const password = data.get('password');
 		if (!username || !password) {
-			return {
-				message: 'Missing Username OR Password for Registration'
-			};
+			// return {
+			// 	message: 'Missing Username OR Password for Registration'
+			// };
+			return fail(400, {
+				username,
+				message: 'Fail Missing Username OR Password for Register'
+			});
 		}
 
 		cookies.set('username', username, { path: '/' });
--- src/routes/named-form-action/+page.svelte
+++ src/routes/named-form-action/+page.svelte
@@ -5,7 +5,7 @@
 
 <form method="post" action="?/login">
 	<p>{form?.message || ''}</p>
-	<input type="text" name="username" placeholder="User Name" />
+	<input type="text" name="username" placeholder="User Name" value={form?.username ?? ''} />
 	<input type="password" name="password" placeholder="Password" />
 	<button type="submit">Named Action Login</button>
 	<button formaction="?/register">Named Action Register</button>
